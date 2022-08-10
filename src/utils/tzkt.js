8551f8403224c56c48de70c38e0e7599bcdb5486
Pushpendra Pratap Singh <pushpendrasingh.19012@gmail.com>
2022-08-10 14:31:21 +0530

    Update tzkt.js

--- src/utils/tzkt.js
+++ src/utils/tzkt.js
@@ -1,11 +1,21 @@
 // TODO 8 - Fetch lottery contract storage
 
 // KT1Mbcm3s69ThWmHf9fxEVtNfV1iLad2CMzR : Health register contract deployed on jakarta testnet
+
+// KT1CzSerSLpKV4FR7gjNFMoKk95qTYpJgc5z : Login contract deployed on jakartanet
+
 import axios from "axios";
 
 export const fetchStorage = async () => {
   const res = await axios.get(
     "https://api.jakartanet.tzkt.io/v1/contracts/KT1Mbcm3s69ThWmHf9fxEVtNfV1iLad2CMzR/storage"
+  );
+  return res.data;
+};
+
+export const fetchStorage1 = async () => {
+  const res = await axios.get(
+    "https://api.jakartanet.tzkt.io/v1/contracts/KT1CzSerSLpKV4FR7gjNFMoKk95qTYpJgc5z/storage"
   );
   return res.data;
 };
