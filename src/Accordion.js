9497df1daa1a83fe0054e02f50c886fefc42cda5
Mumuksh29 <mumukshtayal29@gmail.com>
2022-08-12 15:08:43 +0530

    Accordion-content styling.

--- src/Accordion.js
+++ src/Accordion.js
@@ -9,13 +9,14 @@ const Accordion = ({ title, data1, data2, data3, data4, data5, data6, data7}) =>
         <div>{title}</div>
         <div>{isActive ? '-' : '+'}</div>
       </div>
-      {isActive && <div className="accordion-content">Date: {data1}
-      <br></br>Doctor: {data2}
-      <br></br>Age: {data3}
-      <br></br>Height: {data4}
-      <br></br>Weight: {data5}
-      <br></br>Hospital: {data6}
-      <br></br>Prescription: {data7}
+      {isActive && <div className="accordion-content">
+        {/* Date: {data1} */}
+      <span class='Bold'>Doctor</span> {data2}
+      <br></br><span class='Bold'>Age</span> {data3}
+      <br></br><span class='Bold'>Height</span> {data4}
+      <br></br><span class='Bold'>Weight</span> {data5}
+      <br></br><span class='Bold'>Hospital</span> {data6}
+      <br></br><span class='Bold'>Prescription</span> {data7}
       </div>}
     </div>
   );
